2304290cf2efdf7ab4bcc18edd8b1cd09383b265
Florentina <florentina.k@live.fr>
2017-05-03 14:47:36 +0200

    ok

--- new_script.js
+++ new_script.js
@@ -6,6 +6,13 @@ var score = 0;
 function change(id, value) {
 document.getElementById(id).innerHTML = value;
  }
+    
+    
+  $.getJSON('questions.json', function (data) {
+    console.log(data);
+    console.log(data[0].themeQuestion);
+  });
+
 
 
 function process() {
